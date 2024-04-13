2c16183656629dba1bb946e26c354c539c293fe4
KevinSamarasinghe <166945017+KevinSamarasinghe@users.noreply.github.com>
2024-04-13 19:45:05 +0200

    Add files via upload

---
+++ diadia/src/Test/StanzaTest.java
@@ -0,0 +1,64 @@
+package Test;
+import it.uniroma3.diadia.ambienti.Stanza;
+import it.uniroma3.diadia.attrezzi.Attrezzo;
+
+import static org.junit.Assert.*;
+
+import org.junit.Before;
+import org.junit.Test;
+public class StanzaTest {
+private Stanza stanza;
+	
+	@Before
+	public void SetUp() {
+		this.stanza = new Stanza("STANZA");
+	}
+	
+	@Test
+	public void testHasAttrezzoPresente() {
+		this.stanza.addAttrezzo(new Attrezzo("computer",  50));
+		assertTrue(this.stanza.hasAttrezzo("computer"));
+	}
+	
+	@Test
+	public void testHasAttrezzoNonPresente() {
+		this.stanza.addAttrezzo(new Attrezzo("computer",  50));
+		assertFalse(this.stanza.hasAttrezzo("padella"));
+	}
+	
+	@Test
+	public void testHasAttrezzoStanzaVuota() {
+		assertFalse(this.stanza.hasAttrezzo("cellulare"));
+	}
+	
+	@Test
+	public void testGetStanzaAdiacenteEsistente() {
+		Stanza adiacente = new Stanza("adiacente");
+		stanza.impostaStanzaAdiacente("EST", adiacente);
+		assertEquals(this.stanza.getStanzaAdiacente("EST"),adiacente);
+	}
+	
+	@Test
+	public void testGetStanzaAdiacenteNonEsistente() {
+		assertNull(this.stanza.getStanzaAdiacente("SUD"));
+	}
+	
+	@Test
+	public void testRemoveAttrezzoNonPresente() {
+		stanza.addAttrezzo(new Attrezzo("computer",50));
+		assertFalse(this.stanza.removeAttrezzo(new Attrezzo("padella",70)));
+	}
+	
+	@Test
+	public void testRemoveAttrezzoPresente() {
+		stanza.addAttrezzo(new Attrezzo("computer",50));
+		assertTrue(this.stanza.removeAttrezzo(new Attrezzo("computer",50)));
+	}
+
+
+	@Test
+	public void testRemoveAttrezzoStanzaVuota() {
+		assertFalse(this.stanza.removeAttrezzo(new Attrezzo("computer",50)));
+	}
+
+}
